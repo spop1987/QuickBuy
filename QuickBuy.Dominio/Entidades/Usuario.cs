dde98049b6ccfc1ba762a6fb3d2836d948e511c1
sergio.ontiveros.perez@outlook.com <sergio.ontiveros.perez@outlook.com>
2020-05-06 17:39:50 -0300

    Se crearon las entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        
+        /// <summary>
+        /// Un usuario puede tener ningun o muchos pedidos
+        /// </summary>
+
+        public ICollection<Pedido> Pedidos { get; set; }
+
+    }
+}
